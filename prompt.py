c5b3a4e2f23e8c549978fa08e4e0e91bbc27e9f5
ReturnToFirst <git@privs.dev>
2024-11-11 19:43:08 +0900

    fix(prompt): modify to utilize dataclass decorator

--- prompt.py
+++ prompt.py
@@ -1,72 +1,68 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import toml
+import re
 
-class Template:
-    def __init__(self, task_template:str, language_template:str, specify_language:bool=True):
-        self.src_prompt = ""
-        self.tgt_regex = ""
-
-        self.task_template = task_template
-        self.specify_language = specify_language
-        self.language_template = language_template
-
-    @classmethod
-    def from_dict(cls, config_dict:dict):
-        return cls(
-            task_template=config_dict['task_template'],
-            specify_language=config_dict['specify_language'],
-            language_template=config_dict['language_template']
-        )
-
-class SystemPrompt:
-    def __init__(self, use_system_prompt:bool, system_prompt:str):
-        self.use_system_prompt = use_system_prompt
-        self.system_prompt = system_prompt
-
-    @classmethod
-    def from_dict(cls, config_dict:dict):
-        return cls(
-            use_system_prompt=config_dict['use_system_prompt'],
-            system_prompt=config_dict['system_prompt']
-        )
-
+@dataclass
 class Tag:
-    def __init__(self, src_start:str, src_end:str, tgt_start:str, tgt_end:str):
-        self.src_start = src_start
-        self.src_end = src_end
-        self.tgt_start = tgt_start
-        self.tgt_end = tgt_end
-    
+    src_start: str
+    src_end: str
+    tgt_start: str
+    tgt_end: str
+
     @classmethod
     def from_dict(cls, config_dict:dict):
-        return cls(
-            src_start=config_dict['src_start'],
-            src_end=config_dict['src_end'],
-            tgt_start=config_dict['tgt_start'],
-            tgt_end=config_dict['tgt_end'],
-        )
+        return cls(src_start=config_dict['src_start'],
+                   src_end=config_dict['src_end'],
+                   tgt_start=config_dict['tgt_start'],
+                   tgt_end=config_dict['tgt_end'])
+
+@dataclass
+class Template:
+    task_template: str
+    specify_language: bool
+    language_template: str
+    tag: Tag
+
+    def __post_init__(self):
+        self.src_prompt = f"{self.tag.src_start} {{tgt_text}} {self.tag.src_end}"
+        self.tgt_regex = f"{self.tag.tgt_start}\\s*(.*?)\\s*{self.tag.tgt_end}"
+        self.task_template = self.task_template.format(src_start=self.tag.src_start,
+                                                      src_end=self.tag.src_end,
+                                                      tgt_start=self.tag.tgt_start,
+                                                      tgt_end=self.tag.tgt_end)
+
+    @classmethod
+    def from_dict(cls, config_dict:dict):
+        return cls(task_template=config_dict['task_template'],
+                   specify_language=config_dict['specify_language'],
+                   language_template=config_dict['language_template'],
+                   tag=Tag.from_dict(config_dict['tag']))
+    
+    def get_src_filled_prompt(self, src_text:str):
+        return self.src_prompt.format(src_text=src_text)
+
+@dataclass
+class SystemPrompt:
+    use_system_prompt: bool
+    system_prompt: str
+
+    @classmethod
+    def from_dict(cls, config_dict:dict):
+        return cls(use_system_prompt=config_dict['use_system_prompt'],
+                   system_prompt=config_dict['system_prompt'])
 
 @dataclass
 class Prompt:
-    def __init__(self, system_prompt:SystemPrompt, template:Template, tag:Tag):
-        self.template = template
-        self.system_prompt = system_prompt
-        self.tag = tag
-        self.template.src_prompt = f"{self.tag.src_start} {{tgt_text}} {self.tag.src_end}"
-        self.template.tgt_regex = f"{self.tag.tgt_start}\\s*(.*?)\\s*{self.tag.tgt_end}"
-        
+    template: Template
+    system_prompt: SystemPrompt
+
     @classmethod
-    def from_toml(cls, prompt_file:str=r"prompt.toml"):
+    def from_toml(cls, prompt_file: str = r"prompt.toml"):
         try:
             config_dict = toml.load(prompt_file)
         except Exception as e:
             print(f"Error loading config file: {e}")
             raise e
-        
-        system_prompt = config_dict['system_prompt']
-        template = config_dict['template']
-        tag = config_dict['tag']
 
-        return cls(SystemPrompt.from_dict(system_prompt),
-                   Template.from_dict(template),
-                   Tag.from_dict(tag))
+        return cls(template=Template.from_dict(config_dict['template']),
+                   system_prompt=SystemPrompt.from_dict(config_dict['system_prompt']))
